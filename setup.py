8c66b1db89ef87cab9e87c37fe25211aed4df560
Mikedev <mdv1994@gmail.com>
2022-11-14 14:37:21 +0100

    rename package name

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup
 setup(
     name='pytorch_vqvae',
     version='1.0.0',
-    packages=['model', 'utils', 'dataset', 'callbacks'],
+    packages=['pytorch_vqvae'],
     url='https://github.com/Michedev/VQ-VAE',
     license='MIT',
     author='mikedev',
     author_email='mik3dev@gmail.com',
     description='PyTorch implementation of VQ-VAE',
-    dependencies=['pytorch-lightning', 'torchvision', 'torch', 'tensorguard']
+    dependencies=['pytorch-lightning', 'torchvision', 'torch', 'tensorguard'],
+
 )
